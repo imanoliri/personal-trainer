0f0c5ad07efe7655f4550e2da520c977c3ca733e
Imanol Irizar <imanol.irizar13@gmail.com>
2024-09-12 12:26:33 +0200

    create exercise plan csv from yaml order

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ RESULTS_DIRECTORY = "results"
 
 PERSONAL_DATA_INPUT_FILE = f"{DATA_DIRECTORY}/personal_data_input.csv"
 PERSONAL_DATA_FILE = f"{RESULTS_DIRECTORY}/personal_data.csv"
+EXERCISE_INPUT_FILE = f"{DATA_DIRECTORY}/exercise_plan_input.yml"
+EXERCISE_DATA_FILE = f"{RESULTS_DIRECTORY}/exercise_data.csv"
